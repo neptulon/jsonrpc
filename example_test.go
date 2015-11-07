316e7c2ac0550d20a8cc15d9107c1724017dbf65
Teoman Soygul <tsoygul@gmail.com>
2015-11-07 20:52:17 +0100

    fix repo links

--- example_test.go
+++ example_test.go
@@ -2,7 +2,7 @@ package jsonrpc_test
 
 const debug = false
 
-// Example demonstrating the Devastator server.
+// Example demonstrating the Titan server.
 func Example() {
 
 	// ** Output: Server started
