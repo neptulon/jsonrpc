efac4aea873e3aebe155ac48f0455eb8ebcdab3b
Teoman Soygul <tsoygul@gmail.com>
2015-11-11 14:11:17 +0100

    update project title

--- example_test.go
+++ example_test.go
@@ -2,7 +2,7 @@ package jsonrpc_test
 
 const debug = false
 
-// Example demonstrating the Titan server.
+// Example demonstrating the Neptulon JSON-RPC server.
 func Example() {
 
 	// ** Output: Server started
