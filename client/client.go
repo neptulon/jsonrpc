6c9711baff733cba4957d8e66c7ed5250120a01c
Teoman Soygul <tsoygul@gmail.com>
2015-12-25 14:03:38 +0100

    add Client prototype

--- client/client.go
+++ client/client.go
@@ -1,8 +1,25 @@
 package client
 
-import "github.com/neptulon/neptulon/client"
+import (
+	"github.com/neptulon/cmap"
+	"github.com/neptulon/neptulon/client"
+)
 
 // Client is a Neptulon JSON-RPC client.
 type Client struct {
-	client *client.Client // Inner Neptulon client.
+	client           *client.Client // Inner Neptulon client.
+	inReqMiddleware  []func()
+	inNotMiddleware  []func()
+	outReqMiddleware []func()
+	outNotMiddleware []func()
+}
+
+// ConnID is a randomly generated unique client connection ID.
+func (c *Client) ConnID() string {
+	return c.client.ConnID()
+}
+
+// Session is a thread-safe data store for storing arbitrary data for this connection session.
+func (c *Client) Session() *cmap.CMap {
+	return c.client.Session()
 }
